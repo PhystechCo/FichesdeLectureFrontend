ae19164d58e8706d31bd12dfa91ff09f15262bf3
andres0sorio <osorio.af@gmail.com>
2018-07-16 16:42:48 -0500

    [Angular6] clean project area

--- src/app/fiches/fiche-home/fiche-home.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './fiche-home.component.html',
-  styleUrls: ['./fiche-home.component.css']
-})
-export class FicheHomeComponent { }
